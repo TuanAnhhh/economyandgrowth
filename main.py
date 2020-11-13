025eccbc4d4e6c0ed6e8bdcf67b99b26faa247f0
Tuan Anh Nguyen <tuananh2582000@gmail.com>
2020-11-14 04:23:56 +0700

    moinhat

--- main.py
+++ main.py
@@ -88,6 +88,20 @@ def page2():
     return render_template('page2.html', plot=gfig, tanso1=gtanso1, tansuat1=gtansuat1, tstl1=gtstl1, bang1=[gbang1.to_html(classes='data', header="true")], tanso2=gtanso2, tansuat2=gtansuat2, tstl2=gtstl2, bang2=[gbang2.to_html(classes='data', header="true")], box=gbox, hist=ghist)
 
 
+@app.route('/page4')
+def page4():
+    # bar = char1()
+    bar, fig1, fig2, fig3, fig4, histo = tansuat_tso_tstl_page4()
+    return render_template('page4.html', plot=bar, tanso=fig1, tansuat=fig2, tstl=fig3, box=fig4,  hist=histo)
+
+
+@app.route('/page5')
+def page5():
+    # bar = char1()
+    bar, fig1, fig2, fig3, fig4, histo = tansuat_tso_tstl_page5()
+    return render_template('page4.html', plot=bar, tanso=fig1, tansuat=fig2, tstl=fig3, box=fig4,  hist=histo)
+
+
 def getYear_Value(s):
     rs = col_ref.where('IndicatorName', '==', s).get()
     for item in rs:
@@ -96,7 +110,373 @@ def getYear_Value(s):
     year, value = [i['Year']for i in primary_income_payments], [
         float(i['Value'])for i in primary_income_payments]
     return year, value
+# -----PAGE5--------
 
+
+def tansuat_tso_tstl_page5():
+    year_Trades, value_Trades = getYear_Value(
+        'Trade (% of GDP)')
+    fig = go.Figure()
+    fig.add_trace(go.Bar(x=year_Trades, y=value_Trades, text=value_Trades,
+                         textposition='outside', marker=dict(color='#FF3366')))
+    fig.update_layout(
+        xaxis_title='Năm',
+        yaxis_title='%GDP')
+    k_Trade = round((2*len(value_Trades))**(1/3))
+    h_Trade = math.ceil(
+        (float(max(value_Trades))-float(min(value_Trades)))/k_Trade)
+
+    k1_Trade = round(min(value_Trades)+h_Trade, 2)
+    k2_Trade = round(min(value_Trades)+2*h_Trade, 2)
+    k3_Trade = round(min(value_Trades)+3*h_Trade, 2)
+
+    khoang1_Trade, khoang2_Trade, khoang3_Trade, khoang4_Trade = [], [], [], []
+    for i in value_Trades:
+        if i < min(value_Trades)+h_Trade-0.01:
+            khoang1_Trade.append(i)
+        elif i >= min(value_Trades)+h_Trade and i < min(value_Trades)+2*h_Trade-0.01:
+            khoang2_Trade.append(i)
+        elif i >= min(value_Trades)+2*h_Trade and i < min(value_Trades)+3*h_Trade-0.01:
+            khoang3_Trade.append(i)
+        elif i >= min(value_Trades)+3*h_Trade:
+            khoang4_Trade.append(i)
+    figTanSo = go.Figure()
+    x_Trade = []
+    x_Trade.append('0 - '+str(k1_Trade-0.01))
+    x_Trade.append(str(round(k1_Trade, 2)) + " - " +
+                   str(round(k2_Trade, 2)-0.01))
+    x_Trade.append(str(round(k2_Trade, 2)) + " - " +
+                   str(round(k3_Trade, 2)-0.01))
+    x_Trade.append(str(round(k3_Trade, 2))+" - " +
+                   str(round(max(value_Trades), 2)))
+
+    tanso_Trade = [len(khoang1_Trade), len(khoang2_Trade),
+                   len(khoang3_Trade), len(khoang4_Trade)]
+
+    figTanSo.add_trace(go.Bar(x=x_Trade, y=tanso_Trade, text=tanso_Trade,
+                              textposition='inside', marker=dict(color='#FF3366')))
+    figTanSo.update_layout(
+        xaxis_title='GDP (%)',
+        yaxis_title='Tần số')
+    figTanSo.update_yaxes(range=[0, 15])
+    tanXuat_Trade = [round((i*100)/(sum(tanso_Trade)), 2) for i in tanso_Trade]
+    figTanSuat = go.Figure()
+    figTanSuat.add_trace(go.Pie(labels=x_Trade, values=tanXuat_Trade))
+
+    tanXuatTL_Trade = []
+    s_Trade = 0
+    for i in tanXuat_Trade:
+        s_Trade += i
+        tanXuatTL_Trade.append(s_Trade)
+    figTstl = go.Figure()
+    figTstl.add_trace(go.Scatter(x=x_Trade, y=tanXuatTL_Trade,
+                                 marker=dict(color='#FF3366')))
+    figTstl.update_layout(
+        xaxis_title='GDP %',
+        yaxis_title='Tần xuất tích lũy')
+
+    stt = [i for i in range(1, len(year_Trades)+1)]
+    df_Trades = {'STT': pd.Series(stt, index=stt),
+                 'Year': pd.Series(year_Trades, index=stt),
+                 'GDP(%)': pd.Series(value_Trades, index=stt, dtype='float')}
+    df_Trades = pd.DataFrame(df_Trades)
+    figBox = go.Figure()
+    figBox.add_trace(go.Box(y=df_Trades['GDP(%)'].values,
+                            name='GDP(%) ', boxpoints='all'))
+
+    hist = go.Figure()
+    hist.add_trace(go.Histogram(x=df_Trades['GDP(%)'].values, name='GDP(%)'))
+    hist.update_layout(barmode='overlay')
+    hist.update_traces(opacity=0.7)
+
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    graphJSON1 = json.dumps(figTanSo, cls=plotly.utils.PlotlyJSONEncoder)
+    graphJSON2 = json.dumps(figTanSuat, cls=plotly.utils.PlotlyJSONEncoder)
+    graphJSON3 = json.dumps(figTstl, cls=plotly.utils.PlotlyJSONEncoder)
+    graphBox = json.dumps(figBox, cls=plotly.utils.PlotlyJSONEncoder)
+    histo = json.dumps(hist, cls=plotly.utils.PlotlyJSONEncoder)
+
+    return graphJSON, graphJSON1, graphJSON2, graphJSON3, graphBox, histo
+# ---------PAGE4---------
+
+
+def tansuat_tso_tstl_page4():
+    year_Gross, value_Gross = getYear_Value(
+        'Gross national expenditure (% of GDP)')
+    fig = go.Figure()
+    fig.add_trace(go.Bar(x=year_Gross, y=value_Gross, text=value_Gross,
+                         textposition='inside', marker=dict(color='#339966')))
+    fig.update_layout(
+        xaxis_title='Năm',
+        yaxis_title='USD')
+    fig.update_yaxes(range=[60, 120])
+
+    vl = [i for i in range(153, 180)]
+    k_Gross = round((2*len(value_Gross))**(1/3))
+    h_Gross = math.ceil(
+        (float(max(value_Gross))-float(min(value_Gross)))/k_Gross)
+    k1_Gross = round(min(value_Gross)+h_Gross, 2)
+    k2_Gross = round(min(value_Gross)+2*h_Gross, 2)
+    k3_Gross = round(min(value_Gross)+3*h_Gross, 2)
+
+    khoang1_Gross, khoang2_Gross, khoang3_Gross, khoang4_Gross = [], [], [], []
+    for i in value_Gross:
+        if i < min(value_Gross)+h_Gross-0.01:
+            khoang1_Gross.append(i)
+        elif i >= min(value_Gross)+h_Gross and i < min(value_Gross)+2*h_Gross-0.01:
+            khoang2_Gross.append(i)
+        elif i >= min(value_Gross)+2*h_Gross and i < min(value_Gross)+3*h_Gross-0.01:
+            khoang3_Gross.append(i)
+        elif i >= min(value_Gross)+3*h_Gross:
+            khoang4_Gross.append(i)
+    figTanSo = go.Figure()
+    x_Gross = []
+    x_Gross.append('0 - '+str(k1_Gross-0.01))
+    x_Gross.append(str(round(k1_Gross, 2)) + " - " +
+                   str(round(k2_Gross, 2)-0.01))
+    x_Gross.append(str(round(k2_Gross, 2)) + " - " +
+                   str(round(k3_Gross, 2)-0.01))
+    x_Gross.append(str(round(k3_Gross, 2))+" - " +
+                   str(round(max(value_Gross), 2)))
+
+    tanso_Gross = [len(khoang1_Gross), len(khoang2_Gross),
+                   len(khoang3_Gross), len(khoang4_Gross)]
+
+    figTanSo.add_trace(go.Bar(x=x_Gross, y=tanso_Gross, text=tanso_Gross,
+                              textposition='inside', marker=dict(color='#339966')))
+    figTanSo.update_layout(title='Biểu đồ tần số tổng chi tiêu của Quốc gia',
+                           xaxis_title='USD (%)',
+                           yaxis_title='Tần số')
+    figTanSo.update_yaxes(range=[0, 10])
+
+    tanXuat_Gross = [round((i*100)/(sum(tanso_Gross)), 2) for i in tanso_Gross]
+    figTanSuat = go.Figure()
+    figTanSuat.add_trace(go.Pie(labels=x_Gross, values=tanXuat_Gross))
+
+    tanXuatTL_Gross = []
+    s_Gross = 0
+    for i in tanXuat_Gross:
+        s_Gross += i
+    tanXuatTL_Gross.append(s_Gross)
+
+    figTstl = go.Figure()
+    figTstl.add_trace(go.Scatter(
+        x=x_Gross, y=tanXuatTL_Gross, marker=dict(color='#339966')))
+    figTstl.update_layout(
+        xaxis_title='USD',
+        yaxis_title='Tần xuất tích lũy')
+
+    stt = [i for i in range(1, len(year_Gross)+1)]
+    df_Gross = {'STT': pd.Series(stt, index=stt),
+                'Year': pd.Series(year_Gross, index=stt),
+                'GDP(%) ': pd.Series(value_Gross, index=stt, dtype='float')}
+    df_Gross = pd.DataFrame(df_Gross)
+
+    figBox = go.Figure()
+    figBox.add_trace(go.Box(y=df_Gross['GDP(%) '].values,
+                            name='GDP(%) ', boxpoints='all'))
+
+    hist = go.Figure()
+    hist.add_trace(go.Histogram(x=df_Gross['GDP(%) '].values, name='GDP(%) '))
+    hist.update_layout(barmode='overlay')
+    hist.update_traces(opacity=0.7)
+
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    graphJSON1 = json.dumps(figTanSo, cls=plotly.utils.PlotlyJSONEncoder)
+    graphJSON2 = json.dumps(figTanSuat, cls=plotly.utils.PlotlyJSONEncoder)
+    graphJSON3 = json.dumps(figTstl, cls=plotly.utils.PlotlyJSONEncoder)
+    graphBox = json.dumps(figBox, cls=plotly.utils.PlotlyJSONEncoder)
+    histo = json.dumps(hist, cls=plotly.utils.PlotlyJSONEncoder)
+
+    return graphJSON, graphJSON1, graphJSON2, graphJSON3, graphBox, histo
+# -----------PAGE3-----------
+
+
+def tansuat_tso_tstl_page3():
+    GDP_AFF_year, GDP_AFF_value = getYear_Value(
+        'Agriculture, forestry, and fishing, value added (% of GDP)')
+    Growth_AFF_year, Growth_AFF_value = getYear_Value(
+        'Agriculture, forestry, and fishing, value added (annual % growth)')
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=Growth_AFF_year, y=Growth_AFF_value, mode='lines+markers',
+                             name='% Tăng trưởng hàng năm', marker=dict(color='#fc6d00')))
+    fig.add_trace(go.Bar(x=GDP_AFF_year, y=GDP_AFF_value, text=GDP_AFF_value,
+                         textposition='outside', marker=dict(color='#0099ff'), name='%GDP'))
+    fig.update_layout(title='Biểu đồ %GDP và % tăng trưởng hàng năm của nông, lâm, ngư nghiệp',
+                      xaxis_title='Năm',
+                      yaxis_title='Phần trăm')
+    # Chia khoảng % GDP
+    k_gdp = round((2*len(GDP_AFF_value))**(1/3))
+    h_gdp = math.ceil((float(max(GDP_AFF_value)) -
+                       float(min(GDP_AFF_value)))/k_gdp)
+    k1_gdp = min(GDP_AFF_value)+h_gdp
+    k2_gdp = min(GDP_AFF_value)+2*h_gdp
+    k3_gdp = min(GDP_AFF_value)+3*h_gdp
+
+    khoang1_gdp, khoang2_gdp, khoang3_gdp, khoang4_gdp = [], [], [], []
+    for i in GDP_AFF_value:
+        if i < k1_gdp-0.01:
+            khoang1_gdp.append(i)
+        elif i >= k1_gdp and i < k2_gdp-0.01:
+            khoang2_gdp.append(i)
+        elif i >= k2_gdp and i < k3_gdp-0.01:
+            khoang3_gdp.append(i)
+        elif i >= k3_gdp:
+            khoang4_gdp.append(i)
+
+    # Khoảng
+    khoang_gdp = ['< '+str(k1_gdp-0.01), str(k1_gdp) + " - " + str(k2_gdp-0.01),
+                  str(k2_gdp) + " - " + str(k3_gdp-0.01), '>= ' + str(k3_gdp)]
+    # Tần số
+    tanso_gdp = [len(khoang1_gdp), len(khoang2_gdp),
+                 len(khoang3_gdp), len(khoang4_gdp)]
+    # Tần xuất
+    tanXuat_gdp = [round((i*100)/sum(tanso_gdp), 2)for i in tanso_gdp]
+    # Tần xuất tích lũy
+    tanXuatTL_gdp = []
+    s_gdp = 0
+    for i in tanXuat_gdp:
+        s_gdp = round(s_gdp + i, 2)
+        tanXuatTL_gdp.append(s_gdp)
+
+    # Chia khoảng % Tăng trưởng
+
+    k_growth = round((2*len(Growth_AFF_value))**(1/3))
+    h_growth = math.ceil(
+        (int(max(Growth_AFF_value))-int(min(Growth_AFF_value)))/k_growth)
+
+    k1_growth = min(Growth_AFF_value)+h_growth
+    k2_growth = min(Growth_AFF_value)+2*h_growth
+    k3_growth = min(Growth_AFF_value)+3*h_growth
+
+    khoang1_growth, khoang2_growth, khoang3_growth, khoang4_growth = [], [], [], []
+    for i in Growth_AFF_value:
+        if i < k1_growth-0.01:
+            khoang1_growth.append(i)
+        elif i >= k1_growth and i < k2_growth-0.01:
+            khoang2_growth.append(i)
+        elif i >= k2_growth and i < k3_growth-0.01:
+            khoang3_growth.append(i)
+        elif i >= k3_growth:
+            khoang4_growth.append(i)
+
+    # Khoảng
+    khoang_growth = ['< '+str(round(k1_growth-0.01, 2)), str(round(k1_growth, 2)) + " - " + str(round(
+        k2_growth-0.01, 2)), str(round(k2_growth, 2)) + " - " + str(round(k3_growth-0.01, 2)), '>= ' + str(k3_growth)]
+    # Tần số
+    tanso_growth = [len(khoang1_growth), len(khoang2_growth),
+                    len(khoang3_growth), len(khoang4_growth)]
+    # Tần xuất
+    tanXuat_growth = [round((i*100)/sum(tanso_growth), 2)for i in tanso_growth]
+    # Tần xuất tích lũy
+    tanXuatTL_growth = []
+    s_growth = 0
+    for i in tanXuat_growth:
+        s_growth = round(s_growth + i, 2)
+        tanXuatTL_growth.append(s_growth)
+
+    stt = [i for i in range(1, len(GDP_AFF_year)+1)]
+    df2 = {'STT': pd.Series(stt, index=stt),
+           'Year': pd.Series(GDP_AFF_year, index=stt),
+           'GDP': pd.Series(GDP_AFF_value, index=stt, dtype='float'),
+           'Growth': pd.Series(Growth_AFF_value, index=[i for i in range(1, len(Growth_AFF_year)+1)], dtype='float')}
+    df2 = pd.DataFrame(df2)
+
+    figTanso1 = go.Figure()
+    figTanso1.add_trace(go.Bar(x=khoang_gdp, y=tanso_gdp, text=tanso_gdp,
+                               textposition='inside', marker=dict(color='#0099ff')))
+    figTanso1.update_layout(title='Biểu đồ tần số',
+                            xaxis_title='Phần trăm',
+                            yaxis_title='Tần số')
+# lam toi day---------------------------------
+    figTanSuat1 = go.Figure()
+    figTanSuat1.add_trace(go.Pie(labels=khoang_gdp, values=tanXuat_gdp))
+    figTanSuat1.update_layout(legend=dict(
+        orientation="h",
+        yanchor="bottom",
+        y=-0.5,
+        xanchor="right",
+        x=0.75
+    ))
+
+    figTSTL1 = go.Figure()
+    figTSTL1.add_trace(go.Scatter(x=khoang_gdp, y=tanXuatTL_gdp))
+    figTSTL1.update_layout(
+        xaxis_title='USD',
+        yaxis_title='Tần xuất tích lũy')
+
+    figTanSo2 = go.Figure()
+    figTanSo2.add_trace(go.Bar(x=khoang_growth, y=tanso_growth, text=tanso_growth,
+                               textposition='inside', marker=dict(color='#0099ff')))
+    figTanSo2.update_layout(
+        xaxis_title='Phần trăm',
+        yaxis_title='Tần số')
+
+    figTanSuat2 = go.Figure()
+    figTanSuat2.add_trace(go.Pie(labels=khoang_gdp, values=tanXuat_gdp))
+    figTanSuat2.update_layout(legend=dict(
+        orientation="h",
+        yanchor="bottom",
+        y=-0.5,
+        xanchor="right",
+        x=0.75
+    ))
+    figTSTL2 = go.Figure()
+    figTSTL2.add_trace(go.Scatter(x=khoang_gdp, y=tanXuatTL_gdp))
+    figTSTL2.update_layout(
+        xaxis_title='USD',
+        yaxis_title='Tần xuất tích lũy')
+
+    stt = [j for j in range(1, k_gdp+1)]
+    ddf_gdp = {'Chia khoảng': pd.Series(khoang_gdp, index=stt),
+               'Tần số': pd.Series(tanso_gdp, index=stt, dtype='int'),
+               'Tần xuất': pd.Series(tanXuat_gdp, index=stt, dtype='float'),
+               'Tần xuất tích lũy': pd.Series(tanXuatTL_gdp, index=stt, dtype='float')}
+    ddf_gdp = pd.DataFrame(ddf_gdp)
+
+    ddf_growth = {'Chia khoảng': pd.Series(khoang_growth, index=stt),
+                  'Tần số': pd.Series(tanso_growth, index=stt, dtype='int'),
+                  'Tần xuất': pd.Series(tanXuat_growth, index=stt, dtype='float'),
+                  'Tần xuất tích lũy': pd.Series(tanXuatTL_growth, index=stt, dtype='float')}
+    ddf_growth = pd.DataFrame(ddf_growth)
+
+    stt1 = [i for i in range(1, len(GDP_GS_year)+1)]
+    df3 = {'STT': pd.Series(stt1, index=stt1),
+           'Year': pd.Series(GDP_GS_year, index=stt1),
+           '%GDP': pd.Series(GDP_GS_value, index=stt1, dtype='float'),
+           '%Tăng trưởng': pd.Series(Growth_GS_value, index=[i for i in range(1, len(Growth_GS_year)+1)], dtype='float')}
+    df3 = pd.DataFrame(df3)
+
+    figBox = go.Figure()
+    figBox.add_trace(go.Box(y=df3['%GDP'].values,
+                            name='%GDP', boxpoints='all'))
+    figBox.add_trace(
+        go.Box(y=df3['%Tăng trưởng'].values, name='%Tăng trưởng', boxpoints='all'))
+
+    hist = go.Figure()
+    hist.add_trace(go.Histogram(x=df3['%GDP'].values, name='%GDP'))
+    hist.add_trace(go.Histogram(
+        x=df3['%Tăng trưởng'].values, name='%Tăng trưởng'))
+
+    hist.update_layout(barmode='overlay')
+    hist.update_traces(opacity=0.7)
+
+    gfig = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
+    gtanso1 = json.dumps(figTanSo1, cls=plotly.utils.PlotlyJSONEncoder)
+    gtansuat1 = json.dumps(figTanSuat1, cls=plotly.utils.PlotlyJSONEncoder)
+    gtstl1 = json.dumps(figTSTL1, cls=plotly.utils.PlotlyJSONEncoder)
+    gtanso2 = json.dumps(figTanSo2, cls=plotly.utils.PlotlyJSONEncoder)
+    gtansuat2 = json.dumps(figTanSuat2, cls=plotly.utils.PlotlyJSONEncoder)
+    gtstl2 = json.dumps(figTSTL2, cls=plotly.utils.PlotlyJSONEncoder)
+    gbox = json.dumps(figBox, cls=plotly.utils.PlotlyJSONEncoder)
+
+    ghist = json.dumps(hist, cls=plotly.utils.PlotlyJSONEncoder)
+
+    # bang1 = json.dumps(ddf_gdp, cls=plotly.utils.PlotlyJSONEncoder)
+    # bang2 = json.dumps(ddf_growth, cls=plotly.utils.PlotlyJSONEncoder)
+
+    return gfig, gtanso1, gtansuat1, gtstl1, gtanso2, gtansuat2, gtstl2, gbox, ghist, ddf_gdp, ddf_growth
 # -------------PAGE1--------------------
 
 
@@ -182,22 +562,6 @@ def tansuat_tso_tstl_page1():
     df1 = pd.DataFrame(df1)
     figBox = px.box(df1, y='USD', points="all")
     figBox.update_yaxes(range=[500000000, 17500000000])
-    # moTaBox = [int(i) for i in np.array(df1['USD'].describe())]
-    # motaBox1 = df1['USD'].describe()
-
-    # motaBox1 = {'25%': pd.Series(motaBox1['25%'], index=[1], dtype='long'),
-    #             '50%': pd.Series(motaBox1['50%'], index=[1], dtype='long'),
-    #             '75%': pd.Series(motaBox1['75%'], index=[1], dtype='long'),
-    #             'Min': pd.Series(motaBox1['min'], index=[1], dtype='long'),
-    #             'Max': pd.Series(motaBox1['max'], index=[1], dtype='long'),
-    #             'Mean': pd.Series(motaBox1['mean'], index=[1], dtype='long'),
-    #             'Std': pd.Series(motaBox1['std'], index=[1], dtype='long')}
-
-    # motaBox1 = pd.DataFrame(motaBox1)
-    # histogram
-    # hist_data = [df1['USD'].values]
-    # group_labels = ['USD']
-    # hist = ff.create_distplot(hist_data, group_labels, bin_size=5*10**9)
     hist = px.histogram(df1, x="USD")
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
     graphJSON1 = json.dumps(figTanSo, cls=plotly.utils.PlotlyJSONEncoder)
